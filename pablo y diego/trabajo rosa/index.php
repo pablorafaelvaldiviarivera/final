1c0157c3db4f5b4f9b071865ea41b6f254ddeed4
pablorafaelvaldiviarivera <34502385+pablorafaelvaldiviarivera@users.noreply.github.com>
2017-12-13 14:15:21 -0600

    Add files via upload

---
+++ pablo y diego/trabajo rosa/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8"
+	<title></title>
+
+	<link rel="stylesheet" href="style/index_style.css">
+
+	<script src="JS/jquery-3.2.1.min.js"></script>
+	<script src='js/script.js'></script>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				
+				<a href='reproductor/index.html'><li>Videos</li></a>
+				<a href='carrusel/index.html'><li>Im&aacute;genes</li></a>
+			</ul>
+				
+				<img id='logo' src='pictures/batman.png
+			'>
+
+			<ul>
+				<a href='codigo qr/QR_Code_Mi_galer_a.png'><li>Nosotros</li></a>
+				<a href='https://login.skype.com/login?setlang=es'><li>Contacto</li></a>
+			</ul>
+		</nav>
+	</header>
+
+	<section id='banner'>
+		
+	</section>
+
+	<section id='body'></section>
+</body>
+</html>
